28e2436b23a0b4d974a1ee5c34c73c481c82e234
viniferreira96 <vinicius.ferreira96@hotmail.com>
2015-09-29 21:52:06 -0300

    Classe Jogador(teste)

---
+++ Joga10/src/ClassesPrincipais/Jogador.java
@@ -0,0 +1,16 @@
+package ClassesPrincipais;
+
+public class Jogador {
+	public String Nome;
+	public int dataNascim;
+	public String posicao;
+	public int altura;
+	public float peso;
+	public String timeAtual;
+	public String estado;
+	public String cidade;
+	
+	public double RegJogador;
+	
+	public double passe;
+}
