e618f91e5746e1a6493d72df742b16ff537a5b8d
João Lucas Santos <joao__lucas_santos@hotmail.com>
2015-10-02 14:22:24 -0300

    Tela login Time

--- Joga10/src/GUI/TelaLoginTime.java
+++ Joga10/src/GUI/TelaLoginTime.java
@@ -4,10 +4,16 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import java.awt.Color;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.Font;
 
 public class TelaLoginTime {
 
 	private JFrame frame;
+	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -38,6 +44,39 @@ public class TelaLoginTime {
 	private void initialize() {
 		frame = new JFrame();
 		frame.getContentPane().setBackground(new Color(169, 169, 169));
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblUsurio = new JLabel("Usu\u00E1rio:");
+		lblUsurio.setBounds(54, 92, 46, 14);
+		frame.getContentPane().add(lblUsurio);
+		
+		JLabel lblSenha = new JLabel("Senha:");
+		lblSenha.setBounds(54, 132, 46, 14);
+		frame.getContentPane().add(lblSenha);
+		
+		textField = new JTextField();
+		textField.setBounds(110, 129, 176, 20);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setText(" ");
+		textField_1.setBounds(110, 89, 176, 20);
+		frame.getContentPane().add(textField_1);
+		textField_1.setColumns(10);
+		
+		JButton btnEntrar = new JButton("Entrar");
+		btnEntrar.setBounds(154, 183, 89, 23);
+		frame.getContentPane().add(btnEntrar);
+		
+		JButton btnVoltar = new JButton("Voltar");
+		btnVoltar.setBounds(154, 217, 89, 23);
+		frame.getContentPane().add(btnVoltar);
+		
+		JLabel lblTime = new JLabel("Time");
+		lblTime.setFont(new Font("Impact", Font.PLAIN, 18));
+		lblTime.setBounds(173, 45, 51, 23);
+		frame.getContentPane().add(lblTime);
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
